cfd4444fa545454c07034c1b568de9dacfb3ab90
shahin hossain <shahinhossain3@hotmail.com>
2023-12-20 20:09:22 +0600

    Send Client data to the server and display data on the client

--- __Steps__.js
+++ __Steps__.js
@@ -19,6 +19,14 @@
  *--------------------------------
  * 1. Create a post API on Server side
  * 2. Client side sent data via post
- *
+ * 3. set fetch method inside the fetch option (second parameter)
+ * 4. options will have three things: method
+ * 5. options will have headers: 'content-type': 'application/json'
+ * 6. body: dont forget to send data by JSON.Stringify in the body
+ * 
+ * -------------------------------
+ * on the server side: don't forget to use express.json() middleware
+ * এখন client থেকে যে data পাঠানো হয়েছে সেগুলো server এ সরাসরি পাবে না। এর মধ্যে একটা middleware লাগবে। যেটা হল express.json()
+ * middleware use করার পদ্ধতি হলে app.use(express.json())
  * 
  */
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 5000;
 
 //Middleware 
 app.use(cors());
+app.use(express.json())
 
 const users = [
     { id: 1, name: 'Shahin Hossain', email: 'shahinhossain3@hotmail.com' },
@@ -19,10 +20,18 @@ app.get('/', (req, res) => {
 })
 
 app.get('/users', (req, res) => {
+
     res.send(users);
 })
 app.post('/users', (req, res) => {
+    console.log('post api hitting')
     console.log(req.body)
+    const newUser = req.body; // body এর মধ্যে user থাকবে।
+
+    newUser.id = users.length + 1; // id set করা হয়েছে, নতুন যে user আসবে সে আগের array length থেকে ১ বেশি হবে।
+    users.push(newUser);
+    res.send(newUser);
+
 })
 app.listen(port, () => {
     console.log(`Server is Running on PORT is : ${port}`)
